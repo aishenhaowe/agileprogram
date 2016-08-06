55ddbb0e0ee7b8da3b1bf3400f4f6ca9c7e31002
chenzhen <aishenhaowe@sina.com>
2016-08-06 22:14:45 +0800

    Description:Devided the generatePrime function into three functions BugFixed or Feature:BugFixed

--- GeneratePrimes/src/GeneratePrimes.java
+++ GeneratePrimes/src/GeneratePrimes.java
@@ -2,53 +2,73 @@ import java.util.*;
 
 public class GeneratePrimes
 {
+	private static int s;
+	private static boolean[] f;
+	private static int[] primes;
+	
     public static int [] generatePrimes(int maxValue)
     {
-        //the only valid case
-        if (maxValue >= 2)
+    	if (maxValue < 2)
+    		return new int[0];
+    	else
+    	{
+    		initializeSieve(maxValue);
+    		sieve();
+    		loadPrimes();
+    		
+    		return primes;
+    	}
+    }
+
+    private static void initializeSieve(int maxValue)
+    {
+    	s = maxValue + 1;
+    	f = new boolean[s];
+    	int i;
+        // initialize the array to true
+        for (i = 0; i < s; i++)
+            f[i] = true;
+
+        // get rid of known non-primes
+        f[0] = f[1] = false;
+    }
+    
+    private static void sieve()
+    {
+    	int i;
+    	int j;
+    	
+        for (i = 2; i < Math.sqrt(s) + 1; i++)
         {
-            // declarations
-            int s = maxValue + 1;
-            boolean[] f = new boolean[s];
-            int i;
-
-            // initialize the array to true
-            for (i = 0; i < s; i++)
-                f[i] = true;
-
-            // get rid of known non-primes
-            f[0] = f[1] = false;
-
-            // sieve
-            int j;
-            for (i = 2; i < Math.sqrt(s) + 1; i++)
-            {
+        	if (f[i])
+        	{
                 for (j = 2 * i; j < s; j += i)
                 {
                      f[j] = false;  // multiple is not primes
-                }
-            }
-
-            // how many primes are there
-            int count = 0;
-            for (i = 0; i < s; i++)
-            {
-                 if (f[i])
-                     count++;
-            }
-
-            int [] primes = new int[count];
-
-            // move the primes into the result
-            for (i = 0, j = 0; i < s; i++)
-            {
-                 if (f[i])
-                    primes[j++] = i;
-            }
-
-            return primes;
+                }	        		
+        	}
         }
-        else
-            return new int[0];
+    }
+    
+    private static void loadPrimes()
+    {
+    	int i;
+    	int j;
+    	int count = 0;
+    	
+        for (i = 0; i < s; i++)
+        {
+             if (f[i])
+                 count++;
+        }
+
+        primes = new int[count];
+
+        // move the primes into the result
+        for (i = 0, j = 0; i < s; i++)
+        {
+             if (f[i])
+                primes[j++] = i;
+        }
     }
 }
